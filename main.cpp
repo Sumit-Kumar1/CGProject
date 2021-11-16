ce8e2f7422abc57702f42bfc280ea44712706c8f
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-11-16 18:55:36 +0530

    Added simple window and all header files in header.h

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-	std::cout << "Hello world";
+	sf::Vector2f viewSize(1024, 768);
+	sf::VideoMode vm(viewSize.x, viewSize.y);
+	
+	sf::RenderWindow window(vm, "CG_Project By SUMIT & YASHARTH", sf::Style::Default);
+
+	while (window.isOpen()) {
+
+	}
 	return 0;
 }
