52f741851c6fcc32e066fa0a1390ad256fcb9426
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-11-16 18:53:04 +0530

    Add project files.

---
+++ Header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <conio.h>
+
+// Graphics releated includes
+#include <SFML/Graphics.hpp>
+#include <glm/glm.hpp>
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "Header.h"
+
+int main()
+{
+	std::cout << "Hello world";
+	return 0;
+}
